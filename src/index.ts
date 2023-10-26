98289799a1eafbd3d88e974e3a3504c482c50ef9
Paulo Tosi <tosi.paulo@gmail.com>
2023-10-26 15:20:55 -0300

    Add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express, { Request, Response } from 'express'
+import morgan from "morgan";
 
 const app = express()
 const port = process.env.PORT || 3333
+
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(express.static("public"));
 
 app.get('/', (_req: Request, res: Response) => {
   return res.send('::: Express Typescript on Vercel :::')
