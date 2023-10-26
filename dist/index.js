f63031fe68284468f099e0be8ab01afb0d477718
Paulo Tosi <tosi.paulo@gmail.com>
2023-10-26 16:12:38 -0300

    Config swagger
    Config swagger
    
    Config swagger
    
    Config swagger

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,49 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path = __importStar(require("path"));
+const morgan_1 = __importDefault(require("morgan"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_json_1 = __importDefault(require("./swagger.json"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3333;
+const options = {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.1/swagger-ui.css',
+    customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.1/swagger-ui-bundle.js'
+};
+app.use(express_1.default.static(path.join(__dirname, "public")));
+app.use('/api-docs', (req, res, next) => {
+    res.set('Content-Type', 'text/html');
+    next();
+}, swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default, options));
+app.use(express_1.default.json());
+app.use((0, morgan_1.default)("tiny"));
 app.get('/', (_req, res) => {
     return res.send('::: Express Typescript on Vercel :::');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
-import express, { Request, Response } from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import * as path from 'path';
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from './swagger.json';
 
 const app = express()
-const port = process.env.PORT || 3333
+const port = process.env.PORT || 3333;
+
+const options = {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.1/swagger-ui.css',
+  customJs: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.9.1/swagger-ui-bundle.min.js'
+};
+
+
+
+app.use(express.static("api-docs"));
+
+app.use(
+  '/api-docs',
+  (req: Request, res: Response, next: NextFunction) => {
+    res.set('Content-Type', 'text/html')
+    next();
+  },
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDoc)
+);
 
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(express.static("public"));
 
 app.get('/', (_req: Request, res: Response) => {
   return res.send('::: Express Typescript on Vercel :::')
